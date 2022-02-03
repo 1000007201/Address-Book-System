f2013e92e36c92f5486af3796f833789cd4d3e58
1000007201 <nishant.sharma5504@gmail.com>
2022-02-03 16:20:46 +0530

    [Nishant] Add:New Contact Added

---
+++ AddressBook.cs
@@ -0,0 +1,38 @@
+﻿namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        List<Contacts> address = new List<Contacts>();
+        public void CreateContact()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name:");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            contacts.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address:");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            contacts.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter State:");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter City:");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter Email:");
+            contacts.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip:");
+            contacts.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Created Contact:\n{contacts.FirstName}\n{contacts.LastName}\n{contacts.Address}" +
+                $"{contacts.PhoneNumber}\n{contacts.State}\n{contacts.City}\n{contacts.Email}\n{contacts.Zip}");
+            address.Add(contacts);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Welcome to Address Book!!");
+using AddressBookSystem;
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Enter Value:\n1.Add to Contacts");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateContact();
+            break;
+        case 2:
+            flag = false;
+            break;
+    }
+
+}
